f0e0595cc6c463bca12386396ae59aa51b1025b3
Matteo <matteop18@gmail.com>
2020-01-20 17:33:53 +0100

    aggiunto dropdown aiuto da mettere in un classe generica dropdown

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(document).ready(
         $(this).next('.dropdown').toggleClass('active');
         $(this).next('.dropdown_product').toggleClass('active');
         $(this).next('.dropdown_company').toggleClass('active');
-
+        $(this).next('.dropdown_help').toggleClass('active');
       }
     );
   }
